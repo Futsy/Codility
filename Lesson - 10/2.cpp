b4cbd610a570b20383ccdb7f9cceb862472a6e22
Floris Velleman <florisvelleman@hotmail.com>
2018-11-26 18:32:49 +0100

    Lesson 10 - 2 Oops

--- Lesson - 10/2.cpp
+++ Lesson - 10/2.cpp
@@ -3,7 +3,7 @@
 int solution(int N) {
     // The one most square is the smallest (A closest to B where A * B = area)
     for (int A = std::sqrt(N); A > 0; A--) {
-        if (N % A)
+        if (N % A == 0)
             return 2 * ((N / A) + A);
     }
     return 0;
